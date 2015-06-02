f27c6474cf59323c8b672410779ab1f67c1e9d52
HubertusBrown <hubert.czerniak@gmail.com>
2015-06-03 06:07:52 +0800

    js files adjustment

--- client/components/utils/phases_roles.js
+++ client/components/utils/phases_roles.js
@@ -1,5 +1,17 @@
 'use strict';
 
+var Phases = {
+
+    ANALYSIS : "Analysis",
+    DOCUMENTATION : "Documentation",
+    IMPLEMENTATION: "Implementation",
+    TESTING: "Testing"
+
+};
+
+Phases.array = [Phases.ANALYSIS, Phases.DOCUMENTATION, Phases.IMPLEMENTATION, Phases.TESTING];
+
+
 function role(name, phases, description) {
 
 
